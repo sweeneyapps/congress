96e3d415e8e3367a35ffa9cbf47cfb53aa6a1d3d
Paul <paul@sweeneyapps.com>
2016-07-25 00:18:33 -0700

    clean up code

--- app.js
+++ app.js
@@ -13,37 +13,29 @@ app.get('/', function (req, res) {
 
 // I will refactor this code soon.
 app.get('/govTrack', (request, response) => {
-    govTrack.findRole({ current: true, limit:600, role_type: "senator", sort:"person" }, (err, res) => {
+  govTrack.findRole({ current: true, limit:600, role_type: "senator", sort:"person" }, (err, res) => {
+      var body = "";
+      var heading =  "<h2>United States Senate</h2><hr>";
+      var count = {};
 
-    
-    var body = "";
-
-    var heading =  "<h2>United States Senate</h2><hr>";
-
-    var count = {};
-
-    for (var i = 0; i < res.objects.length; i++) {
-        
-        if (count[res.objects[i].party]) {
+      for (var i = 0; i < res.objects.length; i++) {
+          if (count[res.objects[i].party]) {
              count[res.objects[i].party] += 1;
- } else { 
-    count[res.objects[i].party] = 1;
-
-
-}
+          } else { 
+             count[res.objects[i].party] = 1;
+          }
         
         var name = res.objects[i].person.name;
         body += `<p> ${name} </p>`;        
-}
+      }
+      
       var howMany = "";
-    for (party in count) { howMany += `${party}:${count[party]} / `;    }
-     howMany += "<hr>";
-    var html = `${heading} ${howMany} ${body}`;
-    response.send(html);
+      for (party in count) { howMany += `${party}:${count[party]} / `; }
+      howMany += "<hr>";
+      var html = `${heading} ${howMany} ${body}`;
+      response.send(html);
 
-});
-
-
+  });
 });
 
 
