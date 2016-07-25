ce2c1659a3fb0521737f01fec79245194a2a162e
Paul Sweeney Jr <paul@sweeneyapps.com>
2016-07-24 21:48:17 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var govTrack = require('govtrack-node');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/govTrack', (request, response) => {
+    govTrack.findRole({ current: true, limit:600, role_type: "senator", sort:"person" }, (err, res) => {
+
+    
+    var body = "";
+
+    var heading =  "<h2>United States Senate</h2><hr>";
+
+    var count = {};
+
+    for (var i = 0; i < res.objects.length; i++) {
+        
+        if (count[res.objects[i].party]) {
+             count[res.objects[i].party] += 1;
+ } else { 
+    count[res.objects[i].party] = 1;
+
+
+}
+        
+        var name = res.objects[i].person.name;
+        body += `<p> ${name} </p>`;        
+}
+      var howMany = "";
+    for (party in count) { howMany += `${party}:${count[party]} / `;    }
+     howMany += "<hr>";
+    var html = `${heading} ${howMany} ${body}`;
+    response.send(html);
+
+});
+
+
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
